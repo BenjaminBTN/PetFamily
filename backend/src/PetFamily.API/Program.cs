725dd212529acf60fc74c589f75bc43ee2140c02
Galkin Aleksey <galkin_aleksey@inbox.ru>
2024-12-18 16:45:24 +0300

    add Exception Middleware

--- backend/src/PetFamily.API/Program.cs
+++ backend/src/PetFamily.API/Program.cs
@@ -1,3 +1,4 @@
+using PetFamily.API.Middlewares;
 using PetFamily.Application;
 using PetFamily.Application.Volunteers;
 using PetFamily.Infrastructure;
@@ -18,6 +19,8 @@ builder.Services.AddScoped<IVolunteersRepository, VolunteersRepository>();
 
 
 var app = builder.Build();
+
+app.UseExceptionMiddleware();
 
 // Configure the HTTP request pipeline.
 if(app.Environment.IsDevelopment())
